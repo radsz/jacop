aeda9c7f68f94c9945af310ae1986c1497d8e489
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2016-11-06 00:41:46 +0100

    Adding methods to class MinizincBasedTestHelper

--- src/test/java/org/jacop/MinizincBasedTestsHelper.java
+++ src/test/java/org/jacop/MinizincBasedTestsHelper.java
@@ -3,15 +3,17 @@ package org.jacop;
 import org.jacop.fz.Fz2jacop;
 import org.junit.BeforeClass;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * @author Mariusz Świerkot
@@ -57,6 +59,47 @@ public class MinizincBasedTestsHelper {
         String filePath = new File(relativePath + filename ).getAbsolutePath();
         return Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
     }
+
+    protected static Collection<String> fileReader() throws IOException {
+        FileReader file = new FileReader(relativePath + timeCategory + listFileName);
+        BufferedReader br = new BufferedReader(file);
+        String line = "";
+        List<String> list = new ArrayList<String>();
+        int i = 0;
+        while ((line = br.readLine())!=null) {
+            list.add(i, line);
+            i++;
+        }
+        return list;
+    }
+
+    protected void testExecution() throws IOException {
+        List<String> expectedResult = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+
+        System.out.println("Test file: " + timeCategory + testFilename);
+        expectedResult = expected(timeCategory + testFilename + ".out");
+        result = result(timeCategory + testFilename + ".fzn");
+
+        for (int i = 0, j = 0; i < result.size() || j < expectedResult.size();) {
+            if (i < result.size() && result.get(i).trim().isEmpty() )
+            { i++; continue;}
+            if (j < expectedResult.size() && expectedResult.get(j).trim().isEmpty() )
+            { j++; continue;}
+            if (result.size() == i)
+                fail("\n" + "File path: " + timeCategory + testFilename + ".out " + " gave as a result less textlines that was expected. Expected line " + (j+1) + " not found.");
+            if (expectedResult.size() == j)
+                fail("\n" + "File path: " + timeCategory + testFilename + ".out " + " gave as a result more textlines that was expected. Actual line " + (i + 1) + "not found in expected result");
+
+            assertEquals("\n" + "File path: " + timeCategory + testFilename + ".out " + "\nError line number (expected, actual): (" + (j + 1) + "," + (i + 1) + ")\n",
+                    expectedResult.get(j).trim(), result.get(i).trim());
+            i++; j++;
+        }
+
+
+    }
+
+
 
 
 
