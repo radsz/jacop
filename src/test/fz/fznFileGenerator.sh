546c628b74ccaca52d5ad899987a0730e600aae4
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2017-01-09 14:39:37 +0100

    Small improvements and corrections for script

--- src/test/fz/fznFileGenerator.sh
+++ src/test/fz/fznFileGenerator.sh
@@ -13,36 +13,31 @@ readarray -t arr3 < <(find $z -name \*.fzn)
         ii=${k##*/} # fzn filename with extension
         iii=${ii%.*} # fzn filename without extension
 
-        #basename $(dirname $k)
-
-        #if [[ -z $(find upTo5sec/`basename $(dirname $k)` flakyTests/`basename $(dirname $k)` -name $iii.out 2>/dev/null ) ]]
-        #then
-
         start=$(date +%s ) # start time in seconds
-	    out=$(java -cp ../../../../jacop-*.jar org.jacop.fz.Fz2jacop $k) # Program Fz2jacop generate test result
-	    stop=$(date +%s )  # end time in seconds
+    	   out=$(java -cp ../../../../jacop-*.jar org.jacop.fz.Fz2jacop $k) # Program Fz2jacop generate test result
+        stop=$(date +%s )  # end time in seconds
 
-        echo "Test result for "$k
         echo "$out"
-        echo ""
 	    timesec=$(($stop-$start))
 
 		result=$out
 		diff <(echo $result) <(echo $out)
 
-	i=0
-	while [ $? == 0 -a $i != 4 ];
-	   do
-		out=$(java -cp ../../../../jacop-*.jar org.jacop.fz.Fz2jacop $k) # Program Fz2jacop generate test result
-		   diff <(echo $result) <(echo $out) # diff compare results test to find the difference between two results test
-		    let i++
-		count=$i
+        diffresult=$?
 
-	   done
+    	i=0
+        count=0
+	        while [ $diffresult == 0 -a $i != 1 ];
+	        do
+	    	  out=$(java -cp ../../../../jacop-*.jar org.jacop.fz.Fz2jacop $k) # Program Fz2jacop generate test result
+		      diff <(echo $result) <(echo $out) # diff compare results test to find the difference between two results test
+		      let i++
+		      count=$i
+            done
 
-	if [ $count -eq 4 ];	then
+	if [ $count -eq 1 ];	then
 
-	   if [ $timesec -lt 15 ];then
+	    if [ $timesec -lt 15 ];then
 
 			st=${k#*/*/}
 
@@ -55,7 +50,7 @@ readarray -t arr3 < <(find $z -name \*.fzn)
 
 		fi
 
-	   if [ $timesec -gt 15 ] && [ $timesec -lt 80 ]  ;then
+	    if [ $timesec -gt 15 ] && [ $timesec -lt 80 ]  ;then
 
 			st=${k#*/*/}
 
@@ -67,7 +62,7 @@ readarray -t arr3 < <(find $z -name \*.fzn)
   			mv ${k%%/*}/$(echo "$k" | cut -d / -f 2)/${st%.*}.fzn upTo30sec/${st%.*}.fzn
 		fi
 
-	   if [ $timesec -gt 80 ] && [ $timesec -le 120 ];then
+        if [ $timesec -gt 80 ] && [ $timesec -le 120 ];then
 
 			st=${k#*/*/}
 
@@ -80,7 +75,7 @@ readarray -t arr3 < <(find $z -name \*.fzn)
 		fi
 
 
-	  if [ $timesec -ge 120 ] && [ $timesec -le 600 ];then
+        if [ $timesec -ge 120 ] && [ $timesec -le 600 ];then
 
 			st=${k#*/*/}
 
@@ -92,7 +87,7 @@ readarray -t arr3 < <(find $z -name \*.fzn)
   			mv ${k%%/*}/$(echo "$k" | cut -d / -f 2)/${st%.*}.fzn upTo5min/${st%.*}.fzn
 		fi
 
-	  if [ $timesec -ge 600 ] && [ $timesec -le 1200 ];then
+        if [ $timesec -ge 600 ] && [ $timesec -le 1200 ];then
 
 			st=${k#*/*/}
 
@@ -104,7 +99,7 @@ readarray -t arr3 < <(find $z -name \*.fzn)
   			mv ${k%%/*}/$(echo "$k" | cut -d / -f 2)/${st%.*}.fzn upTo10min/${st%.*}.fzn
 		fi
 
-	  if [ $timesec -ge 1200 ] && [ $timesec -le 5400 ];then
+        if [ $timesec -ge 1200 ] && [ $timesec -le 5400 ];then
 
 			st=${k#*/*/}
 
@@ -116,7 +111,7 @@ readarray -t arr3 < <(find $z -name \*.fzn)
   			mv ${k%%/*}/$(echo "$k" | cut -d / -f 2)/${st%.*}.fzn upTo1hour/${st%.*}.fzn
 		fi
 
-	  if [ $timesec -ge 5400 ] && [ $timesec -le 172800 ];then
+	    if [ $timesec -ge 5400 ] && [ $timesec -le 7200 ];then
 
 			st=${k#*/*/}
 
@@ -126,11 +121,19 @@ readarray -t arr3 < <(find $z -name \*.fzn)
 
 			echo "$out" > above1hour/${st%.*}.out
   			mv ${k%%/*}/$(echo "$k" | cut -d / -f 2)/${st%.*}.fzn above1hour/${st%.*}.fzn
-		fi
+	fi
 
-        else
-            echo "pojedynczy plik"
+    else {
+            echo "Flaky test"
+            st=${k#*/*/}
 
+			if [ ! -d "flakyTests/${st%/*}" ]; then
+		            mkdir -p flakyTests/${st%/*}
+			fi
+
+			echo "$out" > flakyTests/${st%.*}.out
+  			mv ${k%%/*}/$(echo "$k" | cut -d / -f 2)/${st%.*}.fzn flakyTests/${st%.*}.fzn
+         }
 
 	fi
 #fi
@@ -140,6 +143,7 @@ done
 
 
 #main
+echo "Start test: "
 readarray -t arr < <(find test -name \*.mzn );
 
 for i in ${arr[@]}; do
@@ -149,8 +153,9 @@ ii=${i##*/} # mzn filename with extension
 iii=${ii%.*} # mzn filename without extension
 
 # Creating a temporary directory in the same directory as mzn file has resided using the mzn file without extension as the name.
-if [ ! -d "$z/$iii" ]; then
-	mkdir $z/$iii
+
+if [ ! -d "$z/${z#*/}" ]; then
+	mkdir $z/${z#*/}
 
 fi
 
@@ -159,17 +164,16 @@ if [[ -z $(find $z -name \*.dzn) ]]
 then
 
 
-
- if [[ -z $(find upTo5sec/$iii upTo30sec/$iii upTo1min/$iii upTo5min/$iii upTo1hour/$iii above1hour/$iii flakyTests/$iii -name $iii.fzn 2>/dev/null ) || -z $(find upTo5sec/$iii upTo30sec/$iii upTo1min/$iii upTo5min/$iii upTo1hour/$iii above1hour/$iii flakyTests/$iii -name $iii.out 2>/dev/null) ]]
+   if [[ -z $(find upTo5sec/$iii upTo30sec/$iii upTo1min/$iii upTo5min/$iii upTo1hour/$iii above1hour/$iii flakyTests/$iii -name $iii.fzn 2>/dev/null ) || -z $(find upTo5sec/$iii upTo30sec/$iii upTo1min/$iii upTo5min/$iii upTo1hour/$iii above1hour/$iii flakyTests/$iii -name $iii.out 2>/dev/null) ]]
         then
         # Generating fzn files and moving to the temporary directory
-        mzn2fzn -G jacop $i
-        for file in $z/*.fzn; do mv "$file" $z/$iii/"${file/*.fzn/$iii.fzn}"; done
 
+        mzn2fzn -G jacop $i
+        for file in $z/*.fzn; do mv "$file" $z/${z#*/}/"${file/*.fzn/$iii.fzn}"; done
 
         timeCategory
 
-fi
+   fi
 fi
 
 readarray -t arr2 < <(find $z -name \*.dzn)
@@ -179,13 +183,13 @@ for j in ${arr2[@]}; do # j contains a relative path to dzn file.
 	filename=${path##*/}
 
  if [[ -z $(find upTo5sec/$iii upTo30sec/$iii upTo1min/$iii upTo5min/$iii upTo1hour/$iii above1hour/$iii flakyTests/$iii -name $filename.fzn 2>/dev/null )  ||  -z $(find upTo5sec/$iii upTo30sec/$iii upTo1min/$iii upTo5min/$iii upTo1hour/$iii above1hour/$iii flakyTests/$iii -name $filename.out 2>/dev/null ) ]]
-then
+ then
     # Generating fzn files and moving to the temporary directory
     mzn2fzn -G jacop $i -d $j
 	for file in $z/*.fzn; do mv "$file" $z/$iii/"${file/*.fzn/$filename.fzn}"; done
 
-fi
+ fi
 done
 
-    timeCategory
+      timeCategory
 done
