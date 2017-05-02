4bf5e16ac16e3bb89789774a32567fe7ab13d6ac
Krzysztof Kuchcinski <krzysztof.kuchcinski@gmail.com>
2017-05-02 14:59:49 +0200

    Cumulative computing time-table for constant values of durations and resources.

---
+++ src/main/java/org/jacop/constraints/cumulative/CumulativePrimary.java
@@ -0,0 +1,491 @@
+/**
+ * CumulativePrimary.java
+ * This file is part of JaCoP.
+ * <p>
+ * JaCoP is a Java Constraint Programming solver.
+ * <p>
+ * Copyright (C) 2000-2008 Krzysztof Kuchcinski and Radoslaw Szymanek
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * Notwithstanding any other provision of this License, the copyright
+ * owners of this work supplement the terms of this License with terms
+ * prohibiting misrepresentation of the origin of this work and requiring
+ * that modified versions of this work be marked in reasonable ways as
+ * different from the original version. This supplement of the license
+ * terms is in accordance with Section 7 of GNU Affero General Public
+ * License version 3.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+package org.jacop.constraints.cumulative;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jacop.core.IntDomain;
+import org.jacop.core.IntVar;
+import org.jacop.core.Interval;
+import org.jacop.core.IntervalDomain;
+import org.jacop.core.IntervalEnumeration;
+import org.jacop.core.Store;
+import org.jacop.core.Var;
+import org.jacop.constraints.Constraint;
+
+import java.util.BitSet;
+
+/**
+ * CumulativePrimary implements the cumulative constraint using time tabling
+ * algorithm.
+ *
+ * @author Krzysztof Kuchcinski and Radoslaw Szymanek
+ * @version 4.4
+ */
+
+class CumulativePrimary extends Constraint {
+
+    static AtomicInteger idNumber = new AtomicInteger(0);
+
+    static final boolean debug = false, debugNarr = false;
+
+    EventIncComparator<Event> eventComparator = new EventIncComparator<Event>();
+  
+    Store store;
+
+    /*
+     * start times of tasks
+     */
+    IntVar[] start;
+    
+    /*
+     * All durations and resources of the constraint
+     */
+    int[] dur;
+    int[] res;
+    
+    /**
+     * It specifies the limit of the profile of cumulative use of resources.
+     */
+    public IntVar limit;
+
+    
+    /**
+     * It specifies the arguments required to be saved by an XML format as well as
+     * the constructor being called to recreate an object from an XML format.
+     */
+    public static String[] xmlAttributes = {"starts", "durations", "resources", "limit"};
+
+    /**
+     * It creates a cumulative constraint.
+     * @param starts variables denoting starts of the tasks.
+     * @param durations variables denoting durations of the tasks.
+     * @param resources variables denoting resource usage of the tasks.
+     * @param limit the overall limit of resources which has to be used.
+     */
+    public CumulativePrimary(IntVar[] starts, int[] durations, int[] resources, IntVar limit) {
+
+        assert (starts != null) : "Variable in starts list is null";
+        assert (durations != null) : "Variable in durations list is null";
+        assert (resources != null) : "Variable in resource list is null";
+        assert (limit != null) : "Variable limit is null";
+        assert (starts.length == durations.length) : "Starts and durations list have different length";
+        assert (resources.length == durations.length) : "Resources and durations list have different length";
+
+        this.numberArgs = (short) (numberArgs * 3 + 1);
+        this.queueIndex = 2;
+        this.numberId = idNumber.incrementAndGet();
+
+        if (starts.length == durations.length && durations.length == resources.length) {
+
+	    dur = new int[durations.length];
+	    res = new int[resources.length];
+	    start = new IntVar[starts.length];
+	    
+            for (int i = 0; i < starts.length; i++) {
+
+                assert (starts[i] != null) : i + "-th variable in starts list is null";
+                assert (durations[i] >= 0) : i + "-th duration is specified as possibly negative";
+                assert (resources[i] >= 0) : i + "-th resource consumption is specified as possibly negative";
+
+		start[i] = starts[i];
+		
+                if (durations[i] > 0 && resources[i] > 0) {
+		    dur[i] = durations[i];
+		    res[i] = resources[i];
+                } else
+                    throw new IllegalArgumentException("\nDurations and resources must be > 0 in cumulative when defined as constants");
+            }
+
+            if (limit.min() >= 0) {
+                this.limit = limit;
+                numberArgs++;
+            } else {
+                throw new IllegalArgumentException("\nResource limit must be >= 0 in cumulative");
+            }
+        } else {
+            throw new IllegalArgumentException("\nNot equal sizes of Variable vectors in cumulative");
+        }
+    }
+
+    /**
+     * It creates a cumulative constraint.
+     * @param starts variables denoting starts of the tasks.
+     * @param durations variables denoting durations of the tasks.
+     * @param resources variables denoting resource usage of the tasks.
+     * @param limit the overall limit of resources which has to be used.
+     */
+    public CumulativePrimary(ArrayList<? extends IntVar> starts, ArrayList<? extends Integer> durations, ArrayList<? extends Integer> resources,
+        IntVar limit) {
+
+        this(starts.toArray(new IntVar[starts.size()]), durations.stream().mapToInt(i -> i).toArray(),
+	     resources.stream().mapToInt(i -> i).toArray(), limit);
+
+    }
+
+
+    @Override public ArrayList<Var> arguments() {
+
+        ArrayList<Var> variables = new ArrayList<Var>(1);
+
+        for (IntVar s : start)
+            variables.add(s);
+        variables.add(limit);
+        return variables;
+    }
+
+    @Override public void consistency(Store store) {
+
+        do {
+
+            store.propagationHasOccurred = false;
+
+            profileProp();
+
+        } while (store.propagationHasOccurred);
+
+    }
+
+    void profileProp() {
+
+        sweepPruning();
+    }
+
+
+    @Override public int getConsistencyPruningEvent(Var var) {
+
+        // If consistency function mode
+        if (consistencyPruningEvents != null) {
+            Integer possibleEvent = consistencyPruningEvents.get(var);
+            if (possibleEvent != null)
+                return possibleEvent;
+        }
+        return IntDomain.BOUND;
+    }
+
+    @Override public void impose(Store store) {
+
+        for (IntVar s : start) {
+            s.putModelConstraint(this, getConsistencyPruningEvent(s));
+        }
+
+        limit.putModelConstraint(this, getConsistencyPruningEvent(limit));
+
+        store.addChanged(this);
+        store.countConstraint();
+
+        this.store = store;
+    }
+
+    @Override public void removeConstraint() {
+        for (IntVar s : start) 
+            s.removeConstraint(this);
+
+        limit.removeConstraint(this);
+    }
+
+    @Override public boolean satisfied() {
+
+        Task t;
+        boolean sat = true;
+
+        // expensive checking
+        if (limit.singleton()) {
+            int i = 0;
+            while (sat && i < start.length) {
+                sat = sat && start[i].singleton();
+		i++;
+            }
+            return sat;
+        } else
+            return false;
+    }
+
+    @Override public String toString() {
+
+        StringBuffer result = new StringBuffer(id());
+
+        result.append(" : cumulativePrimary([ ");
+        for (int i = 0; i < start.length - 1; i++)
+            result.append("["+start[i]+", ").append(dur[i]+", ").append(res[i]+"], ");
+
+	result.append("["+start[start.length - 1]+", ").append(dur[start.length - 1]+", ").append(res[start.length - 1]+"]");
+
+        result.append(" ]").append(", limit = ").append(limit).append(" )");
+
+        return result.toString();
+
+    }
+
+    @Override public void increaseWeight() {
+        if (increaseWeight) {
+            limit.weight++;
+            for (IntVar s : start) {
+                s.weight++;
+            }
+        }
+    }
+
+    String intArrayToString(int[] a) {
+        StringBuffer result = new StringBuffer("[");
+
+        for (int i = 0; i < a.length; i++) {
+            if (i != 0)
+                result.append(", ");
+            result.append(a[i]);
+        }
+        result.append("]");
+
+        return result.toString();
+    }
+
+    // Sweep algorithm for profile
+    void sweepPruning() {
+
+        Event[] es = new Event[4 * start.length];
+        int limitMax = limit.max();
+
+        boolean mandatoryExists = false;
+        int j = 0;
+        int minProfile = Integer.MAX_VALUE, maxProfile = Integer.MIN_VALUE;
+        for (int i = 0; i < start.length; i++) {
+
+            // mandatory task parts to create profile
+            int min = start[i].max(), max = start[i].min()+dur[i];
+            if (min < max) {
+                es[j++] = new Event(profile, i, min, res[i]);
+                es[j++] = new Event(profile, i, max, -res[i]);
+                minProfile = (min < minProfile) ? min : minProfile;
+                maxProfile = (max > maxProfile) ? max : maxProfile;
+                mandatoryExists = true;
+            }
+        }
+        if (!mandatoryExists)
+            return;
+
+	for (int i=0; i<start.length; i++) {
+            // overlapping tasks for pruning
+            // from start to end
+            int min = start[i].min(); //t.est();
+            int max = start[i].max() + dur[i]; //t.lct();
+	    if (!(min > maxProfile || max < minProfile)) {
+		es[j++] = new Event(pruneStart, i, min, res[i]);
+		es[j++] = new Event(pruneEnd, i, max, -res[i]);
+	    }
+        }
+
+        int N = j;
+        Arrays.sort(es, 0, N, eventComparator);
+
+        if (debugNarr) {
+            System.out.println(Arrays.asList(es));
+            System.out.println("limit.max() = " + limitMax);
+            System.out.println("===========================");
+        }
+
+        BitSet tasksToPrune = new BitSet(start.length);
+        boolean[] inProfile = new boolean[start.length];
+
+        // current value of the profile for mandatory parts
+        int curProfile = 0;
+
+        // used for start variable pruning
+        int[] startExcluded = new int[start.length];
+        Arrays.fill(startExcluded, Integer.MAX_VALUE);
+
+        for (int i = 0; i < N; i++) {
+
+            Event e = es[i];
+            Event ne = null;  // next event
+            if (i < N - 1)
+                ne = es[i + 1];
+
+            switch (e.type()) {
+
+                case profile: // =========== profile event ===========
+
+                    curProfile += e.value();
+                    inProfile[e.index] = (e.value() > 0);
+
+                    if (ne == null || ne.type() != profile || e.date < ne.date()) {
+			// check the tasks for pruning only at the end of all profile events
+			
+                        if (debug)
+                            System.out.println("Profile at " + e.date() + ": " + curProfile);
+
+                        // prune limit variable
+                        if (curProfile > limit.min())
+                            limit.domain.inMin(store.level, limit, curProfile);
+
+                        for (int ti = tasksToPrune.nextSetBit(0); ti >= 0; ti = tasksToPrune.nextSetBit(ti + 1)) {
+                            // TaskView t = taskNormal[ti];
+
+                            int profileValue = curProfile;
+                            if (inProfile[ti])
+                                profileValue -= res[ti];
+
+                            // ========= Pruning start variable
+			    if (startExcluded[ti] == Integer.MAX_VALUE) {
+				if (limitMax - profileValue < res[ti]) {
+				    startExcluded[ti] = e.date() - dur[ti] + 1;
+				}
+			    } else //startExcluded[ti] != Integer.MAX_VALUE
+				if (limitMax - profileValue >= res[ti]) {
+				    // end of excluded interval
+				    int excludeMin = startExcluded[ti];
+				    int excludeMax = e.date() - 1;
+				    
+				    // if (!(startExcluded[ti] > start[ti].max() || e.date() - 1 < start[ti].min())) {
+				    if ((excludeMin <= start[ti].min() && excludeMax >= start[ti].min()) ||
+				    	(excludeMin <= start[ti].max() && excludeMax >= start[ti].max())) {
+					// prune only bounds
+					if (debugNarr)
+					    System.out.print(
+                                                    ">>> CumulativePrimary Profile 1. Narrowed " + start[i] + " \\ " + new IntervalDomain(
+                                                        excludeMin, excludeMax));
+
+					start[ti].domain.inComplement(store.level, start[ti], excludeMin, excludeMax);
+
+					if (debugNarr)
+					    System.out.println(" => " + start[ti]);
+
+				    }
+				    startExcluded[ti] = Integer.MAX_VALUE;
+				}
+                        }
+                    }
+
+                    break;
+
+                case pruneStart:  // =========== start of a task ===========
+                    int profileValue = curProfile;
+                    int ti = e.index;
+
+                    if (inProfile[ti])
+                        profileValue -= res[ti];
+
+                    // ========= for start pruning
+		    if (limitMax - profileValue < res[ti]) 
+			startExcluded[ti] = e.date();
+
+                    tasksToPrune.set(ti);
+                    break;
+
+                case pruneEnd: // =========== end of a task ===========
+                    profileValue = curProfile;
+                    ti = e.index;
+
+                    if (inProfile[ti])
+                        profileValue -= res[ti];
+
+                    // ========= pruning start variable
+		    if (startExcluded[ti] != Integer.MAX_VALUE) {
+			// task ends and we remove forbidden area
+
+			if (!(startExcluded[ti] > start[ti].max() || e.date() < start[ti].min())) {
+			    if (debugNarr)
+				System.out.print(">>> CumulativePrimary Profile 2. Narrowed " + start[ti] + " inMax " + (int) (startExcluded[ti] - 1));
+			    
+			    start[ti].domain.inMax(store.level, start[ti], startExcluded[ti] - 1);
+
+			    if (debugNarr)
+				System.out.println(" => " + start[ti]);
+			    
+			}
+		    }
+
+                    startExcluded[ti] = Integer.MAX_VALUE;
+
+                    tasksToPrune.set(ti, false);
+                    break;
+
+	    default:
+		    throw new RuntimeException("Internal error in " + getClass().getName());
+            }
+        }
+    }
+
+    // event type
+    static final int profile = 0, pruneStart = 1, pruneEnd = 2;
+
+
+    private static class Event {
+        int type;
+        int index;
+        int date;
+        int value;
+
+        Event(int type, int t, int date, int value) {
+            this.type = type;
+            this.index = t;
+            this.date = date;
+            this.value = value;
+        }
+
+        int date() {
+            return date;
+        }
+
+        int type() {
+            return type;
+        }
+
+        int value() {
+            return value;
+        }
+
+        int task() {
+            return index;
+        }
+
+        @Override public String toString() {
+            String result = "(";
+            result += (type == profile) ? "profile, " : (type == pruneStart) ? "pruneStart, " : "pruneEnd, ";
+            result += index + ", " + date + ", " + value + ")\n";
+            return result;
+        }
+    }
+
+
+  private static class EventIncComparator<T extends Event> implements Comparator<T>, java.io.Serializable {
+
+        EventIncComparator() {
+        }
+
+        public int compare(T o1, T o2) {
+            return (o1.date() == o2.date()) ? (o1.type() - o2.type()) : (o1.date() - o2.date());
+        }
+    }
+}
